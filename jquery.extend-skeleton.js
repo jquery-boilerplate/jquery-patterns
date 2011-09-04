7615c4ff7e2dd0f8c047bc3de7bcef38e3282ed5
addyosmani <addyosmani@gmail.com>
2011-09-04 19:59:05 +0100

    adding description to extend pattern

--- jquery.extend-skeleton.js
+++ jquery.extend-skeleton.js
@@ -5,10 +5,19 @@
  * Licensed under the MIT license
  */
 
+/*
+As you'll notice below, we're making use of $.fn.extend to create our plugin rather
+than opting for $.fn.pluginname. This type of structure may be useful if you need
+to add a relatively large number of methods to your plugin. There are however alternatives
+to this that may be better suited, including Alex Sexton's prototypal inheritence pattern 
+which is also included in this repo. 
+*/
 
+
+//the semi colon before function invocation is a safety net against concatenated 
+//scripts and/or other plugins which may not be closed properly.
 ;(function($){
     $.fn.extend({
-
         pluginname: function(options) {
 
             this.defaultOptions = {};
