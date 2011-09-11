aa9234eae18e6df7c160f7a3a5cf61c66d24511d
addyosmani <addyosmani@gmail.com>
2011-09-11 15:47:36 +0100

    minor update to comments for basic plugin boilerplate

--- jquery.basic.plugin-boilerplate.js
+++ jquery.basic.plugin-boilerplate.js
@@ -9,6 +9,7 @@
 // the semi-colon before function invocation is a safety net against concatenated 
 // scripts and/or other plugins which may not be closed properly.
 ;(function ($, window, document, undefined) {
+    
     // undefined is used here as the undefined global variable in ECMAScript 3 is
     // mutable (ie. it can be changed by someone else). undefined isn't really being
     // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
@@ -27,6 +28,11 @@
     // The actual plugin constructor
     function Plugin(element, options) {
         this.element = element;
+
+        // jQuery has an extend method which merges the contents of two or 
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
         this.options = $.extend({}, defaults, this.options);
         
         this._defaults = defaults;
